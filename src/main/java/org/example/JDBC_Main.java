8e96cfe32d4943551320c7bb6e9feed409b66ec3
SachinC8041 <sachinchole619@gmail.com>
2026-03-31 19:41:53 +0530

    Second Commit : Normal CRUD Functionality

--- src/main/java/org/example/JDBC_Main.java
+++ src/main/java/org/example/JDBC_Main.java
@@ -4,30 +4,39 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class JDBC_Main {
+    public static final String url = "jdbc:mysql://localhost:3306/jdbc_student_management";
+    public static final String username = "root";
+    public static final String password = "password1234";
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter 1 to fetch data, Enter 2 to update data:");
+        System.out.println("1. Fetch Data");
+        System.out.println("2. Update Data");
+        System.out.println("3. Delete Data");
+        System.out.println("4. Insert Data");
         int choice = sc.nextInt();
 
-        String url = "jdbc:mysql://localhost:3306/jdbc_student_management";
-        String username = "root";
-        String password = "password1234";
+        try (Connection con = DriverManager.getConnection(url, username, password)) {
 
-        try {
-            Connection con = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection Successful");
-
-            Statement st = con.createStatement();
+            System.out.println("Connection Successful ✅");
 
             switch (choice) {
+
                 case 1:
-                    JDBC_Operations.fetchData(con, st);
+                    JDBC_Operations.fetchData(con);
                     break;
 
                 case 2:
-                    System.out.println("Update feature not implemented yet");
+                    JDBC_Operations.updateData(con, sc);
+                    break;
+
+                case 3:
+                    JDBC_Operations.deleteData(con, sc);
+                    break;
+
+                case 4:
+                    JDBC_Operations.insertData(con, sc);
                     break;
 
                 default:
@@ -37,5 +46,7 @@ public class JDBC_Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        sc.close();
     }
 }
--- src/main/java/org/example/JDBC_Operations.java
+++ src/main/java/org/example/JDBC_Operations.java
@@ -1,25 +1,103 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class JDBC_Operations
 {
-    public static void fetchData(Connection connection, Statement st) throws Exception
-    {
-        ResultSet rs = st.executeQuery("SELECT * FROM Students");
-        while (rs.next()) {
-            System.out.println(
-                    "Id: " + rs.getInt("student_id") +
-                            ", First Name: " + rs.getString("student_first_name") +
-                            ", Last Name: " + rs.getString("student_last_name")
-            );
+    // FETCH
+    public static void fetchData(Connection connection) {
+        try {
+            String query = "SELECT * FROM Students";
+
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(
+                        "Id: " + rs.getInt("student_id") +
+                                ", First Name: " + rs.getString("student_first_name") +
+                                ", Last Name: " + rs.getString("student_last_name")
+                );
+            }
+
+            rs.close();
+            ps.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static void updateData(Connection con, Statement st) throws Exception
+    public static void updateData(Connection con, Scanner sc )
     {
+        try
+        {
+            System.out.println("Enter the ID to update : ");
+            int id = sc.nextInt();
+            sc.nextLine();
 
+            System.out.print("Enter new First Name: ");
+            String fname = sc.nextLine();
+
+            String query = "UPDATE Students SET student_first_name = ? WHERE student_id = ? ";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, fname);
+            ps.setInt(2,id);
+            int rows = ps.executeUpdate();
+            System.out.println(rows + " rows Updated.");
+            ps.close();
+
+        }catch( Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+
+    public static void deleteData(Connection con , Scanner sc)
+    {
+        try{
+            System.out.print("Enter ID to delete: ");
+            int id = sc.nextInt();
+            String query = "DELETE FROM Students WHERE student_id = ?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setInt(1, id);
+            int rows = ps.executeUpdate();
+            System.out.println(rows + " record deleted.");
+
+            ps.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //Insert dAta
+    public static void insertData(Connection con, Scanner sc) {
+        try {
+            sc.nextLine();
+            System.out.print("Enter First Name: ");
+            String fname = sc.nextLine();
+
+            System.out.print("Enter Last Name: ");
+            String lname = sc.nextLine();
+
+
+            String query = "INSERT INTO Students (student_first_name , student_last_name ) VALUES (?, ?)";
+
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, fname);
+            ps.setString(2, lname);
+
+            int rows = ps.executeUpdate();
+            System.out.println(rows + " record inserted.");
+
+            ps.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
